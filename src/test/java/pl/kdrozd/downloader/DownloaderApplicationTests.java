7e17596955d69a7d4c9e4ce9423da984ba87c96e
Konrad Drozd <drozdkonrad@gmail.com>
2022-09-20 19:09:52 +0200

    Generate project with Spring Initializr

---
+++ src/test/java/pl/kdrozd/downloader/DownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kdrozd.downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
