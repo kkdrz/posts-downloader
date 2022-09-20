79e9609a56ae182adb24cfdb6dc435d0c4665bd9
Konrad Drozd <drozdkonrad@gmail.com>
2022-09-20 19:46:35 +0200

    Pretty print JSON

---
+++ src/main/java/pl/kdrozd/downloader/config/ObjectMapperConfig.java
@@ -0,0 +1,17 @@
+package pl.kdrozd.downloader.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        return objectMapper;
+    }
+}
